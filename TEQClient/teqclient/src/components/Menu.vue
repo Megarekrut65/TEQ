7fe9c73491e08e2b4fb6d26c7f236d852dc5d5cb
Megarekrut65 <bandalak28@gmail.com>
2024-11-18 15:45:35 +0200

    Added template page for client

---
+++ TEQClient/teqclient/src/components/Menu.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav id="navbar">
+    <div class="main-menu stellarnav">
+      <ul class="menu-list">
+        <li class="menu-item active"><a href="#home">Home</a></li>
+        <li class="menu-item"><a href="#featured-books" class="nav-link">Featured</a></li>
+        <li class="menu-item"><a href="#popular-books" class="nav-link">Popular</a></li>
+        <li class="menu-item"><a href="#special-offer" class="nav-link">Offer</a></li>
+        <li class="menu-item"><a href="#latest-blog" class="nav-link">Articles</a></li>
+        <li class="menu-item"><a href="#download-app" class="nav-link">Download App</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
